ae60e01f7d134dd5a6f17efb46d5eabca525ff12
Yiannis Karalis <ioankaralis@gmail.com>
2019-04-16 14:42:47 +0300

    "add the changes"

--- js/app.js
+++ js/app.js
@@ -1,16 +1,110 @@
-jQuery(init);
+jQuery(document).ready(init);
+ 
 
-function init($){
+//Same as: jQuery(document).ready(init);
+ 
 
-    let options = {
+//Our application goes here:
 
-        url: "../data.json",
-        success: jsonHandler
-    }
-        function jsonHandler(data){
+function init() {
 
-            console.log(data);
-        }
-    
-   $.ajax(options);
-    }
+ let options = {
+
+ url: "data.json",
+
+ success: jsonHandler
+
+ }
+ 
+
+ function removeDups(names) {
+
+ let unique = {};
+
+ names.forEach(function (i) {
+
+ if (!unique[i]) { unique[i] = true; }
+
+ });
+
+ return Object.keys(unique);
+
+ }
+ 
+
+ function showOptions(listOfCities){
+
+ //1)get datalist
+
+ let datalist = document.querySelector("#cities");
+
+ console.log(datalist); //Testing
+ 
+
+ //2)loop over unique cities array
+
+ listOfCities.map(addOption);
+ 
+
+ 
+
+ function addOption(city){
+
+ let option = `<option value="${city}"></option>` 
+
+ datalist.innerHTML += option;
+
+ }
+ 
+
+ // 2.1)Inside Loop:
+
+ // --Create option element
+
+ // --Add city value
+
+ // --Add to datalist(innerHTML)
+
+ }
+ 
+
+ function jsonHandler(data) {
+ 
+
+ function getCity(hotel) {
+
+ return hotel.city;
+
+ //console.log(hotel.city);
+
+ }
+ 
+
+ let entries = data[1].entries;
+
+ let cities = entries.map(getCity);
+
+ let uniqueCities = removeDups(cities);
+ 
+
+ //console.log(entries); //Array [ {HOTEL} x 4 ]
+ 
+
+ // console.log(uniqueCities);
+
+ showOptions(uniqueCities);
+ 
+
+ 
+
+ }
+ 
+
+ $.ajax(options);
+
+}
+ 
+
+//Alternative,no global vars at all:
+
+//jQuery (function init()){ ... });
